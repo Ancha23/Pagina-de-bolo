149363e975dd0b8f4d5b8e74812ebe7394e845a9
Ancha23 <anchacatarina@gmail.com>
2024-07-30 15:03:52 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { route } from "./routes";
+import React from 'react';
+
 
 export const App = () => {
+ 
   return (
     <>
+ 
       <RouterProvider router={route} />
     </>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,13 +2,22 @@ import { Header } from "../components/header/header"
 import { Footer } from "../components/footer/footer"
 import { Slider } from "../components/sliders/slider";
 import { BolosDeAniversario } from "./receitas dos bolos/bolos de aniversario/aniversarios";
-import { MagnifyingGlass, CaretDown} from "@phosphor-icons/react";
+import { MagnifyingGlass, CaretDown, Gift} from "@phosphor-icons/react";
+import { motion } from "framer-motion";
+import React, { useState } from 'react';
 
 export const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return(
     <>
     <Header/>
-    
+   <div>
+    <div>
+      <div className="">
+        <img src="" alt="" />
+        <p>hjdjd</p>
+      </div>
+    </div>
     <div className="flex flex-col items-center mt-10">
   <div className="flex items-center space-x-4">
     <img 
@@ -35,11 +44,68 @@ export const Home = () => {
   <p className=" flex mr-10">Datas Especias <CaretDown  className="mt-2 ml-2" size={16}/></p>
   <p className="flex mr-10">Sobre-nos <CaretDown  className="mt-2 ml-2" size={16}/></p>
   <p className="flex mr-10">Loja <CaretDown  className="mt-2 ml-2" size={16}/></p>
-</div>
-    <Slider/>
-    <BolosDeAniversario/>
-    <div className="bg-black/20">
+</div></div>
 
+<section>
+      {/* Seção que contém o ícone de presente */}
+      {!isOpen && (
+        <motion.section
+ className="flex justify-center items-center mt-36"
+          animate={{
+            y: [0, -10, 0], // Movimento vertical
+            scale: 1, // Tamanho constante
+            rotate: 0, // Sem rotação
+          }}
+          transition={{
+            duration: 0.5, // Duração do salto
+            repeat: Infinity, // Repetir infinitamente
+            repeatType: 'loop', // Tipo de repetição
+            ease: 'easeInOut', // Tipo de easing
+          }}
+          onClick={() => setIsOpen(true)} // Adiciona um handler para o clique
+          style={{ cursor: 'pointer' }} // Adiciona um cursor de ponteiro para indicar que é clicável
+        >
+          <Gift size={300}  weight="bold"
+          className="text-pink-300 "
+          />
+        </motion.section>
+      )}
+
+      {/* Seção que contém o conteúdo adicional */}
+      {isOpen && (
+        <div className="flex justify-center items-center mt-8">
+          <motion.div 
+            initial={{ y: '-100%', opacity: 0 }}
+            animate={{ y: '0', opacity: 1 }}
+            transition={{ duration: 0.9, delay: 0.1 }}
+            className=""
+          >
+            <img 
+              src="https://img.freepik.com/fotos-gratis/smoothie-de-morango-com-waffle-minusculo-chocolate-e-fatias-de-morangos-no-prato-branco-na-superficie-azul-escura_114579-33846.jpg?ga=GA1.1.860888004.1721808221&semt=ais_hybrid" 
+              alt="Smoothie de Morango"
+            />
+          </motion.div>
+
+          <motion.div 
+            initial={{ x: '100%', opacity: 0 }}
+            animate={{ x: '0', opacity: 1 }}
+            transition={{ duration: 0.9, delay: 0.9 }}
+            className="ml-8 bg-pink-400/40 p-4 rounded"
+          >
+            <h4>Bolo no ponte</h4>
+            <ol>
+              <li>1 massa de pão-de-ló assadeira média</li>
+              <li>1/2 l de leite mais um copo para molhar o bolo</li>
+            </ol>
+          </motion.div>
+        </div>
+      )}
+    </section>
+
+
+   <Slider/>
+    <div className="bg-black/20">
+ <BolosDeAniversario/>
     </div>
     <Footer/>
     </>
